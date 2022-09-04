7dd08afee07dd17daadf36b04a2818b9c50683e1
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2022-09-04 01:30:43 -1000

    Issue-035 :  About Us Cards
    Created cards to showcase our team and what Alahele is about.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Card, ListGroup, ListGroupItem} from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
@@ -15,6 +15,84 @@ const Landing = () => (
         <p>Now get to work and modify this app!</p>
       </Col>
 
+    </Row>
+
+    <Row xs={1} md={2} className="g-4">
+      <div>
+        <Card>
+          <Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                <Card.Title>About Us</Card.Title>
+              </ListGroupItem>
+              <Card.Text>
+                <p></p>
+                Aloha! Welcome to Alahele. Our team is comprised of UH Manoa Students who are dedicated to helping you find your alahele throught the legislative process.
+
+              </Card.Text>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      </div>
+
+      <div>
+        <Card>
+          <Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                <Card.Title>Alahele Defined</Card.Title>
+              </ListGroupItem>
+              <Card.Text>
+                <p></p>
+                <p>[ala hele] noun</p>
+
+                <p>1. Pathway, route, road, way to go, itinerary, trail, highway, means of transportation.
+                  ʻO ka pono koʻu ala hele, my course is righteousness.</p>
+
+              </Card.Text>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      </div>
+
+
+      <div>
+        <Card>
+          <Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                <Card.Title>Get To Know Our Team</Card.Title>
+              </ListGroupItem>
+              <Card.Text>
+
+                <p>Zak Gilbert</p>
+                <p>Cathy Kim</p>
+                <p>Steven Le</p>
+                <p>William Liang</p>
+                <p>Yong-Sung Masuda</p>
+                <p>Lise Nilsen</p>
+                <p>Kristine Rivera</p>
+                <p>Hyunjun Song</p>
+              </Card.Text>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      </div>
+
+      <div>
+        <Card>
+          <Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                <Card.Title>Ask Our Team</Card.Title>
+              </ListGroupItem>
+              <Card.Text>
+                Under Maintenance
+              </Card.Text>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      </div>
     </Row>
   </Container>
 );
