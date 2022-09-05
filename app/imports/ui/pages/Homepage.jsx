9c4afc6c32fea2d46c111fa920382e3e572a9412
eric-song1773 <hsong2@hawaii.edu>
2022-09-04 15:17:48 -1000

    Created Homepage.jsx and added 4 Clickable Cards
    The first page is still the homepage.jsx (temporarily. Added 4 Cards that are clickable which hopefully can redirect to different pages in the future).

--- app/imports/ui/pages/Homepage.jsx
+++ app/imports/ui/pages/Homepage.jsx
@@ -1,12 +1,67 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Card, Container, Row } from 'react-bootstrap';
 // import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
-const Homepage = () => (
-  <>
-    <Button variant="primary">Primary</Button>{' '}
-  </>
-);
+const Homepage = () => {
 
+  function handleClick() {
+    console.log('in cardClick');
+  }
+  return (
+    <Container>
+      <div className="mt-5">
+        <Row>
+          <Card className="bg-dark text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '15rem' }} border="primary">
+            <Card.Body>
+              <Card.Title>View Existing Bills</Card.Title>
+              <Card.Text>
+                Description of View Exisiting Bills
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Row>
+      </div>
+
+      <div className="mt-5">
+        <Row>
+          <Card className="bg-dark text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '15rem' }} border="primary">
+            <Card.Body>
+              <Card.Title>Go to My Email</Card.Title>
+              <Card.Text>
+                Description of Go to My Email
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Row>
+      </div>
+
+      <div className="mt-5">
+        <Row>
+          <Card className="bg-dark text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '15rem' }} border="primary">
+            <Card.Body>
+              <Card.Title>View Archived Bills</Card.Title>
+              <Card.Text>
+                Description of View Archieved Bills
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Row>
+      </div>
+
+      <div className="mt-5">
+        <Row>
+          <Card className="bg-dark text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '15rem' }} border="primary">
+            <Card.Body>
+              <Card.Title>Add a Testimony</Card.Title>
+              <Card.Text>
+                Description of Add a Testimony
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Row>
+      </div>
+    </Container>
+  );
+};
 export default Homepage;
