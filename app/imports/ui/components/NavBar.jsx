f9630d7d70a10c7e5640f085e5b33610e5f0bba6
William Liang <williamliang808@gmail.com>
2022-09-05 00:48:57 -1000

    Makes navbar reactive to window width

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -17,40 +17,46 @@ const NavBar = () => {
     <div>
       {currentUser ? ([null]) :
         ([
-          <Navbar bg="light" expand="lg">
+          <Navbar bg="light" variant="light" expand="md">
             <Container>
-              <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
-                <img width="50px" src="../images/hidoe-logo.png" alt="hidoe logo" />
-              </Navbar.Brand>
-              <h4 className="me-auto">Ala Hele</h4>
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_IN} as={NavLink} to="/signin">
-                <button id="login" type="button" className="btn btn-outline-secondary">Login</button>
-              </Nav.Link>
+              <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
+              <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
+                <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
+                  <img width="50px" src="../images/hidoe-logo.png" alt="hidoe logo" />
+                </Navbar.Brand>
+                <h4 className="me-auto">Ala Hele</h4>
+                <Nav.Link id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_IN} as={NavLink} to="/signin">
+                  <button id="login" type="button" className="btn btn-outline-secondary">Login</button>
+                </Nav.Link>
+              </Navbar.Collapse>
             </Container>
           </Navbar>,
         ])}
       {currentUser ? ([
-        <Navbar bg="light" variant="light">
+        <Navbar bg="light" variant="light" expand="md">
           <Container>
-            <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
-              <img width="50px" src="../images/hidoe-logo.png" alt="hidoe logo" />
-            </Navbar.Brand>
-            <h4 className="me-auto">Ala Hele</h4>
-            <Nav className="me-auto justify-content-center flex-grow-1 pe-3">
-              <Nav.Link href="#Option 1">Option 1</Nav.Link>
-              <Nav.Link href="#Option 2">Option 2</Nav.Link>
-              <Nav.Link href="#Option 3">Option 3</Nav.Link>
-              {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
-                [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin Option</Nav.Link>,
-                  <NavDropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} title="Admin Option 2: Dropdown" key="manage-dropdown">
-                    <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} to="/manage-database"><CloudDownload /> Admin Option 2: Dropdown</NavDropdown.Item>
-                  </NavDropdown>]
-              ) : ''}
-            </Nav>
-            <NavDropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
-              <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/changeme"><PersonCircle /> View Profile</NavDropdown.Item>
-              <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/signout"><BoxArrowRight /> Sign out</NavDropdown.Item>
-            </NavDropdown>
+            <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
+            <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
+              <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
+                <img width="50px" src="../images/hidoe-logo.png" alt="hidoe logo" />
+              </Navbar.Brand>
+              <h4 className="me-auto">Ala Hele</h4>
+              <Nav className="me-auto justify-content-center flex-grow-1 pe-3">
+                <Nav.Link href="#Option 1">Option 1</Nav.Link>
+                <Nav.Link href="#Option 2">Option 2</Nav.Link>
+                <Nav.Link href="#Option 3">Option 3</Nav.Link>
+                {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
+                  [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin Option</Nav.Link>,
+                    <NavDropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} title="Admin Option 2: Dropdown" key="manage-dropdown">
+                      <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} to="/manage-database"><CloudDownload /> Admin Option 2: Dropdown</NavDropdown.Item>
+                    </NavDropdown>]
+                ) : ''}
+              </Nav>
+              <NavDropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
+                <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/changeme"><PersonCircle /> View Profile</NavDropdown.Item>
+                <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/signout"><BoxArrowRight /> Sign out</NavDropdown.Item>
+              </NavDropdown>
+            </Navbar.Collapse>
           </Container>
         </Navbar>,
       ]) : ([null])}
