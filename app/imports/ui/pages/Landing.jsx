ebe5a12e5a4495744c6ecda162d74e155f53ceb3
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2022-09-04 20:42:07 -1000

    Issue-31 Update About Us Page
    Add form and buttons

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row, Card, ListGroup, ListGroupItem} from 'react-bootstrap';
+import { Col, Container, Image, Row, Card, ListGroup, ListGroupItem, Form, FloatingLabel, Button} from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
@@ -27,7 +27,7 @@ const Landing = () => (
               </ListGroupItem>
               <Card.Text>
                 <p></p>
-                Aloha! Welcome to Alahele. Our team is comprised of UH Manoa Students who are dedicated to helping you find your alahele throught the legislative process.
+                Aloha! Welcome to Alahele. Our team is comprised of UH Manoa Students who are dedicated to helping you find your alahele through the legislative process.
 
               </Card.Text>
             </ListGroup>
@@ -64,15 +64,16 @@ const Landing = () => (
                 <Card.Title>Get To Know Our Team</Card.Title>
               </ListGroupItem>
               <Card.Text>
-
-                <p>Zak Gilbert</p>
-                <p>Cathy Kim</p>
-                <p>Steven Le</p>
-                <p>William Liang</p>
-                <p>Yong-Sung Masuda</p>
-                <p>Lise Nilsen</p>
-                <p>Kristine Rivera</p>
-                <p>Hyunjun Song</p>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Zak Gilbert</li>
+                  <li className="list-group-item">Cathy Kim</li>
+                  <li className="list-group-item">Steven Le</li>
+                  <li className="list-group-item">William Liang</li>
+                  <li className="list-group-item">Yon-Sung Masuda</li>
+                  <li className="list-group-item">Lise Nilsen</li>
+                  <li className="list-group-item">Kristine Rivera</li>
+                  <li className="list-group-item">Hyunjun Song</li>
+                </ul>
               </Card.Text>
             </ListGroup>
           </Card.Body>
@@ -82,14 +83,30 @@ const Landing = () => (
       <div>
         <Card>
           <Card.Body>
+
             <ListGroup className="list-group-flush">
               <ListGroupItem>
-                <Card.Title>Ask Our Team</Card.Title>
+                <Card.Title>Get To Know Our Team</Card.Title>
               </ListGroupItem>
-              <Card.Text>
-                Under Maintenance
-              </Card.Text>
-            </ListGroup>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control placeholder="name@example.com" disabled />
+            </Form.Group>
+
+              <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                <Form.Control
+                    as="textarea"
+                    placeholder="Leave a comment here"
+                    style={{ height: '200px' }}
+                />
+              </FloatingLabel>
+
+              <Button variant="outline-secondary" size="sm">Submit</Button>{' '}
+
+
+          </ListGroup>
+
           </Card.Body>
         </Card>
       </div>
