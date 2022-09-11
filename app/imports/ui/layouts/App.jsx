4666eef51a069046f0a42afb37ced31baf5506dc
eric-song1773 <hsong2@hawaii.edu>
2022-09-10 20:52:46 -1000

    (#5) Reverted back the default page from Homepage to Landing

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -24,7 +24,7 @@ const App = () => (
     <div className="d-flex flex-column min-vh-100">
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/" element={<Landing />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signout" element={<SignOut />} />
