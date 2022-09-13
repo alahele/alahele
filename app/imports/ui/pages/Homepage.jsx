4c994d66a27cba2bdb235355a718183b888f7a12
eric-song1773 <hsong2@hawaii.edu>
2022-09-12 23:07:29 -1000

    (#7) Completely reworked the homepage design.
    The numbers of buttons reduced from 4 to 3. Now the homepage has a background image with texts.

--- app/imports/ui/pages/Homepage.jsx
+++ app/imports/ui/pages/Homepage.jsx
@@ -8,40 +8,39 @@ const Homepage = () => {
     console.log('Redirecting...');
   }
   return (
-    <Container className="card-container">
-      <Row>
-        <Col>
-          <Card className="bg-primary text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '30rem' }}>
-            <Card.Body>
-              <Card.Title>View Existing Measures</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="bg-danger text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '30rem' }}>
-            <Card.Body>
-              <Card.Title>Go to My Email</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-3">
-        <Col>
-          <Card className="bg-success text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '30rem' }}>
-            <Card.Body>
-              <Card.Title>View My Notifications</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="bg-warning text-white" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '30rem' }}>
-            <Card.Body>
-              <Card.Title>View Hearing Schedule</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div className="homepage-background">
+      <Container className="text-background">
+        <h1 className="homepage-text">View Measures</h1>
+        <h3 className="homepage-subtext">Explore existing measures, hearings, and testimonies here</h3>
+      </Container>
+      <div className="buttons-background">
+        <Container className="card-container">
+          <Row>
+            <Col>
+              <Card className="bg-warning text-black" border="warning" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '20rem' }}>
+                <Card.Body>
+                  <Card.Title>View Measures</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="bg-warning text-black" border="warning" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '20rem' }}>
+                <Card.Body>
+                  <Card.Title>View Hearings</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="bg-warning text-black" border="warning" onClick={() => handleClick()} style={{ cursor: 'pointer', height: '20rem' }}>
+                <Card.Body>
+                  <Card.Title>View Testimonies</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 };
 export default Homepage;
