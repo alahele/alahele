1d925361ad54c7c64e2abd238fe0e8786de84fd7
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2022-09-15 00:06:41 -1000

    fixes #70 user profile testcafe
    tests user profile page shows

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -5,6 +5,7 @@ import { signInPage } from './signin.page';
 import { navBar } from './navbar.component';
 import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 import { billListPage } from './billlist.page.js';
+import { userProfile} from './userprofile.page';
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
@@ -53,3 +54,9 @@ test('Test that admin page show up', async () => {
   await navBar.logout();
 });
 
+test('Test that user profile page shows up', async () => {
+  await navBar.gotoSignInPage();
+  await signInPage.signin(credentials.username, credentials.password);
+  await navBar.gotoUserProfilePage();
+  await userProfile.isDisplayed();
+});
