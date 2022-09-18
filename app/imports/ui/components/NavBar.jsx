0f038a7e23b268193102e9add75bde0f4644b199
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2022-09-18 12:28:57 -1000

    issue #91 Redesigned landing to signin page & navbar
    New problems: navbar won't hide after signout and my eslint is not working again...

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -14,61 +14,41 @@ const NavBar = () => {
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
   return (
-    <div>
-      {currentUser ? ([null]) :
-        ([
-          <Navbar bg="light" variant="light" expand="md">
+      <div>
+        {(currentUser) ? ([
+          <Navbar style={{ height: '60px' }} collapseOnSelect expand="lg" className="my-2 navbar navbar-expand-lg navbar-light bg-light">
             <Container>
-              <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
+              <Navbar.Brand id={COMPONENT_IDS.NAVBAR_HOME_PAGE} as={NavLink} to="/home">
+                <img width="100px" src="../images/alahele.png" alt="ala hele logo" />
+              </Navbar.Brand>
+              <Navbar.Toggle className="nav" aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
               <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
-                <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
-                  <img width="50px" src="../images/hidoe-logo.png" alt="hidoe logo" />
-                </Navbar.Brand>
-                <h4 className="me-auto">
-                  Ala Hele
-                </h4>
-                <Nav.Link id={COMPONENT_IDS.NAVBAR_LOGIN} as={NavLink} to="/signin">
-                  <button id="login" type="button" className="btn btn-outline-secondary me-auto">Login</button>
-                </Nav.Link>
+                <Nav className="nav-fill me-auto ms-5 flex-grow-1 pe-3">
+
+                  <Nav.Link id={COMPONENT_IDS.NAVBAR_BILL_LIST} href="/bill-list">Bills List</Nav.Link>
+                  <Nav.Link id={COMPONENT_IDS.NAVBAR_TESTIMONY_LIST} href="/testimony-list">Testimony List</Nav.Link>
+                  <Nav.Link id={COMPONENT_IDS.NAVBAR_HEARING_LIST} href="/hearing-list">Hearing List</Nav.Link>
+                  <Nav.Link id={COMPONENT_IDS.NAVBAR_CREATE_TESTIMONY} href="/create-testimony">Create Testimony</Nav.Link>
+                  {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
+                      [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin Option</Nav.Link>,
+                        <NavDropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} title="Admin Option 2: Dropdown" key="manage-dropdown">
+                          <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} to="/manage-database">
+                            <CloudDownload /> Admin Option 2: Dropdown</NavDropdown.Item>
+                        </NavDropdown>]
+                  ) : ''}
+                </Nav>
+                <NavDropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
+                  <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_USER_PROFILE} as={NavLink} to="/user-profile">
+                    Profile
+                    <Person />
+                  </NavDropdown.Item>
+                  <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/">Sign out <BoxArrowRight /></NavDropdown.Item>
+                </NavDropdown>
               </Navbar.Collapse>
             </Container>
           </Navbar>,
-        ])}
-      {currentUser ? ([
-        <Navbar bg="light" variant="light" expand="md">
-          <Container>
-            <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
-            <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
-              <Navbar.Brand id={COMPONENT_IDS.NAVBAR_HOME_PAGE} as={NavLink} to="/home">
-                <img width="50px" src="../images/hidoe-logo.png" alt="hidoe logo" />
-                <h4 className="me-auto">
-                  Ala Hele
-                </h4>
-              </Navbar.Brand>
-              <Nav className="me-auto ms-5 flex-grow-1 pe-3">
-                <Nav.Link id={COMPONENT_IDS.NAVBAR_BILL_LIST} href="/bill-list">Bills List</Nav.Link>
-                <Nav.Link id={COMPONENT_IDS.NAVBAR_TESTIMONY_LIST} href="/testimony-list">Testimony List</Nav.Link>
-                <Nav.Link id={COMPONENT_IDS.NAVBAR_HEARING_LIST} href="/hearing-list">Hearing List</Nav.Link>
-                <Nav.Link id={COMPONENT_IDS.NAVBAR_CREATE_TESTIMONY} href="/create-testimony">Create Testimony</Nav.Link>
-                {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
-                  [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin Option</Nav.Link>,
-                    <NavDropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} title="Admin Option 2: Dropdown" key="manage-dropdown">
-                      <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} to="/manage-database"><CloudDownload /> Admin Option 2: Dropdown</NavDropdown.Item>
-                    </NavDropdown>]
-                ) : ''}
-              </Nav>
-              <NavDropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} title={currentUser}>
-                <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_USER_PROFILE} as={NavLink} to="/user-profile">
-                  Profile
-                  <Person />
-                </NavDropdown.Item>
-                <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} as={NavLink} to="/signout">Sign out <BoxArrowRight /></NavDropdown.Item>
-              </NavDropdown>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>,
-      ]) : ([null])}
-    </div>
+        ]) : ([null])}
+      </div>
   );
 };
 
--- app/imports/ui/pages/SignIn.jsx
+++ app/imports/ui/pages/SignIn.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
+import { Alert, Col, Row, Card, Button } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, TextField } from 'uniforms-bootstrap5';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
@@ -43,33 +43,36 @@ const SignIn = () => {
   }
   // Otherwise return the Login form.
   return (
-    <Container id={PAGE_IDS.SIGN_IN} className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center">
-            <h2>Login to your account</h2>
-          </Col>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)}>
-            <Card>
-              <Card.Body>
-                <TextField id={COMPONENT_IDS.SIGN_IN_FORM_EMAIL} name="email" placeholder="E-mail address" />
+      <div id={PAGE_IDS.SIGN_IN} className="my-auto mx-auto">
+        <Card className="signin px-5 py-5">
+          <Row className="justify-content-md-center">
+            <Col md="6" className="px-5 text-center ">
+              <Card.Title><img width="300px" src="../images/alahele.png" alt="Ala hele Logo"/></Card.Title>
+              <Card.Subtitle className="text-muted">DOE Legislative Bill Tracker</Card.Subtitle>
+            </Col>
+            <Col md="6" className="px-5">
+              <AutoForm schema={bridge} onSubmit={data => submit(data)}>
+                <TextField name="email" placeholder="E-mail address" />
                 <TextField id={COMPONENT_IDS.SIGN_IN_FORM_PASSWORD} name="password" placeholder="Password" type="password" />
                 <ErrorsField />
-                <SubmitField id={COMPONENT_IDS.SIGN_IN_FORM_SUBMIT} />
-              </Card.Body>
-            </Card>
-          </AutoForm>
-          {error === '' ? (
-            ''
-          ) : (
-            <Alert variant="danger">
-              <Alert.Heading>Login was not successful</Alert.Heading>
-              {error}
-            </Alert>
-          )}
-        </Col>
-      </Row>
-    </Container>
+                <div className="d-grid gap-2">
+                  <Button variant="dark" type="submit" size="sm" id={COMPONENT_IDS.SIGN_IN_FORM_SUBMIT}>Submit</Button>
+
+                  <Button variant="light" size="sm">Create an account</Button>
+                </div>
+              </AutoForm>
+              {error === '' ? (
+                  ''
+              ) : (
+                  <Alert variant="danger">
+                    <Alert.Heading>Login was not successful</Alert.Heading>
+                    {error}
+                  </Alert>
+              )}
+            </Col>
+          </Row>
+        </Card>
+      </div>
   );
 };
 
