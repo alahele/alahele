e75ebfc60488b4340b40d0cc1c335c70138b1f4a
eric-song1773 <hsong2@hawaii.edu>
2022-09-28 00:09:18 -1000

    (issue-88) Created a template for extracting the data from MongoDB

--- app/imports/ui/pages/BillList.jsx
+++ app/imports/ui/pages/BillList.jsx
@@ -1,188 +1,206 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Container } from 'react-bootstrap';
 import { MDBBadge, MDBTable, MDBTableHead, MDBTableBody, MDBPagination, MDBPaginationLink, MDBPaginationItem } from 'mdb-react-ui-kit';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Measures } from '../../api/measure/MeasureCollection';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
 /* A simple static component to render some text for the BillList page. */
-const BillList = () => (
-  <Container id={PAGE_IDS.BILL_LIST}>
-    <MDBTable align="middle">
-      <MDBTableHead>
-        <tr>
-          <th scope="col">Bill #</th>
-          <th scope="col">Bill/Resolution</th>
-          <th scope="col">Office</th>
-          <th scope="col">Status</th>
-          <th scope="col">Action</th>
-          <th scope="col">Hearing Date</th>
-          <th scope="col">Hearing Type</th>
-          <th scope="col">Act #</th>
-          <th scope="col">Link</th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        <tr>
-          <td>
-            <p className="fw-normal mb-1">HB433</p>
-          </td>
-          <td>
-            super long bill title that is used for testing to see how it changes the table. can it go
-            longer who knows? testing one two three four five six seven eight nine ten eleven twelve
-            thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty one twenty two twenty three
-            twenty four twenty five twenty six twenty seven twenty eight twenty nine thirty thirty one thirty two
-            thirty three thirty four thirty five thirty six thirty seven thirty eight thirty nine forty
-            forty one forty two forty three forty four forty five forty six forty seven forty eight forty nine fifty
-            fifty one fifty two fifty three fifty four fifty five fifty six fifty seven fifty eight fifty nine sixty
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              OFO, OITS
-            </p>
-          </td>
-          <td>
-            <MDBBadge className="fw-normal mb-1" color="success">
-              Passed/Adopted
-            </MDBBadge>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              Testimony
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              03/17/2022
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              Hearing
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              233
-            </p>
-          </td>
-          <td>
-            <a href="/individualbill" type="button" className="btn btn-primary btn-sm">View</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <p className="fw-normal mb-1">HB433</p>
-          </td>
-          <td>
-            super long bill title that is used for testing to see how it changes the table. can it go
-            longer who knows? testing one two three four five six seven eight nine ten eleven twelve
-            thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty one twenty two twenty three
-            twenty four twenty five twenty six twenty seven twenty eight twenty nine thirty thirty one thirty two
-            thirty three thirty four thirty five thirty six thirty seven thirty eight thirty nine forty
-            forty one forty two forty three forty four forty five forty six forty seven forty eight forty nine fifty
-            fifty one fifty two fifty three fifty four fifty five fifty six fifty seven fifty eight fifty nine sixty
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              OFO, OITS
-            </p>
-          </td>
-          <td>
-            <MDBBadge className="fw-normal mb-1" color="success">
-              Passed/Adopted
-            </MDBBadge>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              Testimony
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              03/17/2022
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              Hearing
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              233
-            </p>
-          </td>
-          <td>
-            <a href="/individualbill" type="button" className="btn btn-primary btn-sm">View</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <p className="fw-normal mb-1">HB433</p>
-          </td>
-          <td>
-            super long bill title that is used for testing to see how it changes the table. can it go
-            longer who knows? testing one two three four five six seven eight nine ten eleven twelve
-            thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty one twenty two twenty three
-            twenty four twenty five twenty six twenty seven twenty eight twenty nine thirty thirty one thirty two
-            thirty three thirty four thirty five thirty six thirty seven thirty eight thirty nine forty
-            forty one forty two forty three forty four forty five forty six forty seven forty eight forty nine fifty
-            fifty one fifty two fifty three fifty four fifty five fifty six fifty seven fifty eight fifty nine sixty
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              OFO, OITS
-            </p>
-          </td>
-          <td>
-            <MDBBadge className="fw-normal mb-1" color="success">
-              Passed/Adopted
-            </MDBBadge>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              Testimony
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              03/17/2022
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              Hearing
-            </p>
-          </td>
-          <td>
-            <p className="fw-normal mb-1">
-              233
-            </p>
-          </td>
-          <td>
-            <a id={COMPONENT_IDS.INDIVIDUAL_BILL_BUTTON} href="/individualbill" type="button" className="btn btn-primary btn-sm">View</a>
-          </td>
-        </tr>
-      </MDBTableBody>
-    </MDBTable>
-    <MDBPagination className="mb-0 justify-content-center">
-      <MDBPaginationItem>
-        <MDBPaginationLink href="#">Previous</MDBPaginationLink>
-      </MDBPaginationItem>
-      <MDBPaginationItem>
-        <MDBPaginationLink href="#">1</MDBPaginationLink>
-      </MDBPaginationItem>
-      <MDBPaginationItem>
-        <MDBPaginationLink href="#">2</MDBPaginationLink>
-      </MDBPaginationItem>
-      <MDBPaginationItem>
-        <MDBPaginationLink href="#">3</MDBPaginationLink>
-      </MDBPaginationItem>
-      <MDBPaginationItem>
-        <MDBPaginationLink href="#">Next</MDBPaginationLink>
-      </MDBPaginationItem>
-    </MDBPagination>
-  </Container>
-);
+const BillList = () => {
+  const mapData = this.props.measures;
+  console.log(mapData);
+  return (
+    <Container id={PAGE_IDS.BILL_LIST}>
+      <MDBTable align="middle">
+        <MDBTableHead>
+          <tr>
+            <th scope="col">Bill #</th>
+            <th scope="col">Bill/Resolution</th>
+            <th scope="col">Office</th>
+            <th scope="col">Status</th>
+            <th scope="col">Action</th>
+            <th scope="col">Hearing Date</th>
+            <th scope="col">Hearing Type</th>
+            <th scope="col">Act #</th>
+            <th scope="col">Link</th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>
+          <tr>
+            <td>
+              <p className="fw-normal mb-1">HB433</p>
+            </td>
+            <td>
+              super long bill title that is used for testing to see how it changes the table. can it go
+              longer who knows? testing one two three four five six seven eight nine ten eleven twelve
+              thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty one twenty two twenty three
+              twenty four twenty five twenty six twenty seven twenty eight twenty nine thirty thirty one thirty two
+              thirty three thirty four thirty five thirty six thirty seven thirty eight thirty nine forty
+              forty one forty two forty three forty four forty five forty six forty seven forty eight forty nine fifty
+              fifty one fifty two fifty three fifty four fifty five fifty six fifty seven fifty eight fifty nine sixty
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                OFO, OITS
+              </p>
+            </td>
+            <td>
+              <MDBBadge className="fw-normal mb-1" color="success">
+                Passed/Adopted
+              </MDBBadge>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                Testimony
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                03/17/2022
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                Hearing
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                233
+              </p>
+            </td>
+            <td>
+              <a href="/individualbill" type="button" className="btn btn-primary btn-sm">View</a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p className="fw-normal mb-1">HB433</p>
+            </td>
+            <td>
+              super long bill title that is used for testing to see how it changes the table. can it go
+              longer who knows? testing one two three four five six seven eight nine ten eleven twelve
+              thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty one twenty two twenty three
+              twenty four twenty five twenty six twenty seven twenty eight twenty nine thirty thirty one thirty two
+              thirty three thirty four thirty five thirty six thirty seven thirty eight thirty nine forty
+              forty one forty two forty three forty four forty five forty six forty seven forty eight forty nine fifty
+              fifty one fifty two fifty three fifty four fifty five fifty six fifty seven fifty eight fifty nine sixty
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                OFO, OITS
+              </p>
+            </td>
+            <td>
+              <MDBBadge className="fw-normal mb-1" color="success">
+                Passed/Adopted
+              </MDBBadge>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                Testimony
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                03/17/2022
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                Hearing
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                233
+              </p>
+            </td>
+            <td>
+              <a href="/individualbill" type="button" className="btn btn-primary btn-sm">View</a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p className="fw-normal mb-1">HB433</p>
+            </td>
+            <td>
+              super long bill title that is used for testing to see how it changes the table. can it go
+              longer who knows? testing one two three four five six seven eight nine ten eleven twelve
+              thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty one twenty two twenty three
+              twenty four twenty five twenty six twenty seven twenty eight twenty nine thirty thirty one thirty two
+              thirty three thirty four thirty five thirty six thirty seven thirty eight thirty nine forty
+              forty one forty two forty three forty four forty five forty six forty seven forty eight forty nine fifty
+              fifty one fifty two fifty three fifty four fifty five fifty six fifty seven fifty eight fifty nine sixty
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                OFO, OITS
+              </p>
+            </td>
+            <td>
+              <MDBBadge className="fw-normal mb-1" color="success">
+                Passed/Adopted
+              </MDBBadge>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                Testimony
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                03/17/2022
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                Hearing
+              </p>
+            </td>
+            <td>
+              <p className="fw-normal mb-1">
+                233
+              </p>
+            </td>
+            <td>
+              <a id={COMPONENT_IDS.INDIVIDUAL_BILL_BUTTON} href="/individualbill" type="button" className="btn btn-primary btn-sm">View</a>
+            </td>
+          </tr>
+        </MDBTableBody>
+      </MDBTable>
+      <MDBPagination className="mb-0 justify-content-center">
+        <MDBPaginationItem>
+          <MDBPaginationLink href="#">Previous</MDBPaginationLink>
+        </MDBPaginationItem>
+        <MDBPaginationItem>
+          <MDBPaginationLink href="#">1</MDBPaginationLink>
+        </MDBPaginationItem>
+        <MDBPaginationItem>
+          <MDBPaginationLink href="#">2</MDBPaginationLink>
+        </MDBPaginationItem>
+        <MDBPaginationItem>
+          <MDBPaginationLink href="#">3</MDBPaginationLink>
+        </MDBPaginationItem>
+        <MDBPaginationItem>
+          <MDBPaginationLink href="#">Next</MDBPaginationLink>
+        </MDBPaginationItem>
+      </MDBPagination>
+    </Container>
+  );
+};
 
-export default BillList;
+export default withTracker(() => {
+  // Get access to Measure.
+  const subscription = Meteor.subscribe(Measures.subscribeMeasures());
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Request documents
+  const measures = Measures.collection.find({}).fetch();
+  return {
+    measures,
+    ready,
+  };
+})(BillList);
