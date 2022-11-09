b3ba4f638e12a07b4c372b8cd5fe2284e2bb6bee
zakgilbert <gilbertz@hawaii.edu>
2022-11-09 11:45:00 -1000

    Fix useTracker error on UpcomingHearings

--- app/imports/ui/components/UpcomingHearings.jsx
+++ app/imports/ui/components/UpcomingHearings.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import '/client/style.css';
-import { Card, Container, Row, Col, Button } from 'react-bootstrap';
-import LoadingSpinner from './LoadingSpinner';
-import CountDown from './CountDown';
+import React from "react";
+import "/client/style.css";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
+import { useTracker } from "meteor/react-meteor-data";
+import { Hearings } from "../../api/hearing/HearingCollection";
+import UpcomingHearing from "../components/UpcomingHearing";
+import LoadingSpinner from "./LoadingSpinner";
+import CountDown from "./CountDown";
 
 const UpcomingHearings = () => {
-  /*
     const { ready, hearings } = useTracker(() => {
         const subscription = Hearings.subscribeHearings();
         const rdy = subscription.ready();
@@ -15,49 +17,17 @@ const UpcomingHearings = () => {
             ready: rdy
         };
     }, []);
-    const hearing = hearings[0];
-    console.log(hearings);
-    hearing.dateTime = "Friday, October 27, 2022 2:00 pm";
 
-     */
-  const date = 'Friday, October 27, 2022 2:00 pm';
-  return (true ? (
-    <Container>
-      <Card>
-        <Card.Header>Upcoming Hearings</Card.Header>
-      </Card>
-      <Card>
-        <Card.Header>
-          <Row>
-            Hearing Number
-          </Row>
-          <Row>
-            Date
-          </Row>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>
-            Description
-          </Card.Text>
-          <Row>
-            <Col>
-              <Card.Text>
-                <CountDown dateTimeStr={date} />
-              </Card.Text>
-            </Col>
-            <Col>
-              <Button>View Hearing</Button>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    </Container>
-  ) : <LoadingSpinner message="Loading Hearings" />);
+
+    return (true ? (
+        <Container>
+            <Card>
+                <Card.Header>Upcoming Hearings</Card.Header>
+            </Card>
+            {hearings.map((hearing, idx) => (
+                <UpcomingHearing hearing={hearing}/>
+            ))}
+        </Container>
+    ) : <LoadingSpinner message="Loading Hearings"/>);
 };
-/*
-{hearings.map((hearing, idx) =>(
-    <UpcomingHearing hearing={hearing}/>
-))}
- */
-
 export default UpcomingHearings;
