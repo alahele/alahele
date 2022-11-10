64aa3a0d000e9572fe071379c835b65520925b6e
Steven Le <77819979+sle417@users.noreply.github.com>
2022-11-10 13:35:40 -1000

    temporary fix for testimony list

--- app/imports/ui/pages/TestimonyList.jsx
+++ app/imports/ui/pages/TestimonyList.jsx
@@ -92,8 +92,101 @@ const TestimonyList = () => {
         </tr>
       </MDBTableHead>
       <MDBTableBody>
-        {testimony.map((testimony) => <TestimonyItem key={testimony._id} testimony={testimony}/>)}
-        {sortedTestimonies.map((testimony) => <TestimonyItem key={testimony._id} testimony={testimony}/>)}
+        {/*{testimony.map((testimony) => <TestimonyItem key={testimony._id} testimony={testimony}/>)}*/}
+        {/*{sortedTestimonies.map((testimony) => <TestimonyItem key={testimony._id} testimony={testimony}/>)}*/}
+        <tr>
+          <td>
+            <div className="d-flex align-items-center">
+              <img
+                  src="https://cbs6albany.com/resources/media/393c9e11-c6ed-4cdb-b3db-0610871899f6-large3x4_AP20022064296602.jpg?1587153484149"
+                  alt=""
+                  style={{ width: '45px', height: '45px' }}
+                  className="rounded-circle"
+              />
+              <div className="ms-3">
+                <p className="fw-bold mb-1">David Ige</p>
+                <p className="text-muted mb-0">david.ige@hawaii.gov</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className="fw-normal mb-1">06/09/2020</p>
+          </td>
+          <td>
+            <p className="fw-normal mb-1">SB 2486, SD1</p>
+          </td>
+          <td>The Department of Education (Department) supports SB 2486, SD1 and respectfully provides comments. Systematic data collection improves our</td>
+          <td>
+            <MDBBadge color="success" pill>
+              Approved
+            </MDBBadge>
+          </td>
+          <td>
+            <a href="/individual-testimony" type="button" className="btn btn-primary btn-sm">View</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className="d-flex align-items-center">
+              <img
+                  src="https://cbs6albany.com/resources/media/393c9e11-c6ed-4cdb-b3db-0610871899f6-large3x4_AP20022064296602.jpg?1587153484149"
+                  alt=""
+                  style={{ width: '45px', height: '45px' }}
+                  className="rounded-circle"
+              />
+              <div className="ms-3">
+                <p className="fw-bold mb-1">David Shmige</p>
+                <p className="text-muted mb-0">david.shmige@hawaii.gov</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className="fw-normal mb-1">06/23/2020</p>
+          </td>
+          <td>
+            <p className="fw-normal mb-1">SB 2486, SD1</p>
+          </td>
+          <td>The Department of Education (Department) supports SB 2486, SD1 and respectfully provides comments. Systematic data collection improves our</td>
+          <td>
+            <MDBBadge color="success" pill>
+              Approved
+            </MDBBadge>
+          </td>
+          <td>
+            <a href="/individual-testimony" type="button" className="btn btn-primary btn-sm">View</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className="d-flex align-items-center">
+              <img
+                  src="https://cbs6albany.com/resources/media/393c9e11-c6ed-4cdb-b3db-0610871899f6-large3x4_AP20022064296602.jpg?1587153484149"
+                  alt=""
+                  style={{ width: '45px', height: '45px' }}
+                  className="rounded-circle"
+              />
+              <div className="ms-3">
+                <p className="fw-bold mb-1">Da Vid Ige</p>
+                <p className="text-muted mb-0">da.vid.ige@hawaii.gov</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className="fw-normal mb-1">06/23/2020</p>
+          </td>
+          <td>
+            <p className="fw-normal mb-1">SB 2486, SD1</p>
+          </td>
+          <td>The Department of Education (Department) supports SB 2486, SD1 and respectfully provides comments. Systematic data collection improves our</td>
+          <td>
+            <MDBBadge color="success" pill>
+              Approved
+            </MDBBadge>
+          </td>
+          <td>
+            <a href="/individual-testimony" type="button" className="btn btn-primary btn-sm">View</a>
+          </td>
+        </tr>
       </MDBTableBody>
     </MDBTable>
     <MDBPagination className="mb-0 justify-content-center">
